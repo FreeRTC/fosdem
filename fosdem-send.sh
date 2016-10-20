e57a187edd943321b9fa898964ceb28771febef1
Daniel Pocock <daniel@pocock.pro>
2016-10-20 10:15:18 +0200

    Update script

--- fosdem-send.sh
+++ fosdem-send.sh
@@ -2,15 +2,15 @@
 
 #MSG_BODY=fosdem-cfp.txt
 MSG_BODY=talk-reminder.txt
-#SUBJECT="[CFP] FOSDEM 2016, RTC devroom, speakers, volunteers neeeded"
-SUBJECT="[CFP] reminder! FOSDEM RTC dev-room talks: deadline Friday"
+SUBJECT="[CFP] FOSDEM 2017, RTC devroom, speakers, volunteers neeeded"
+#SUBJECT="[CFP] reminder! FOSDEM RTC dev-room talks: deadline Friday"
 
 cat fosdem-lists.txt | while read ;
 do
 
   cat $MSG_BODY | \
     mail -s "$SUBJECT" \
-        -a "From: Daniel Pocock <daniel@pocock.pro>" \
+        -a "From: FOSDEM RTC Team <fosdem-rtc-admin@freertc.org>" \
         -a "Content-Type: text/plain; charset=UTF-8" \
         "$REPLY"
 
