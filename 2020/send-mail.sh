a033d2d5ecbb109ff0c7fc52d78b7fe3e37a7974
Saúl Ibarra Corretgé <s@saghul.net>
2019-10-23 09:43:44 +0200

    2020: here we go!

---
+++ 2020/send-mail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MSG_BODY=cfp.txt
+#MSG_BODY=talk-reminder.txt
+SUBJECT="[CFP] FOSDEM 2019, RTC devroom, speakers, volunteers neeeded"
+#SUBJECT="[CFP] reminder! FOSDEM RTC dev-room talks: deadline Friday"
+
+cat mailing-lists.txt | cut -f1 -d, | while read ;
+do
+
+  cat $MSG_BODY | \
+    mail -s "$SUBJECT" \
+        -r "fosdem-rtc-admin@freertc.org" \
+        -a "From: FOSDEM RTC Team <fosdem-rtc-admin@freertc.org>" \
+        -a "Content-Type: text/plain; charset=UTF-8" \
+        "$REPLY"
+
+done
+
