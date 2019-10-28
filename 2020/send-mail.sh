b4fe3d69c8ca596d9e1a32f1b91a8e14959c7699
Daniel Pocock <daniel@pocock.pro>
2019-10-28 18:03:47 +0100

    Include a References header in messages, so they appear as a thread for people receiving the message through multiple lists

--- 2020/send-mail.sh
+++ 2020/send-mail.sh
@@ -7,14 +7,26 @@ MSG_BODY=cfp.txt
 SUBJECT="[CFP] FOSDEM 2020, RTC devroom, speakers, volunteers neeeded"
 #SUBJECT="[CFP] reminder! FOSDEM RTC dev-room talks: deadline Sunday"
 
-cat mailing-lists.txt | cut -f1 -d, | while read ;
-do
+THE_UUID=`uuidgen`
+MESSAGE_ID="<${THE_UUID}@freertc.org>"
 
-  cat $MSG_BODY | \
+cat $MSG_BODY | \
     mail -s "$SUBJECT" \
         -r "fosdem-rtc-admin@freertc.org" \
         -a "From: FOSDEM RTC Team <fosdem-rtc-admin@freertc.org>" \
         -a "Content-Type: text/plain; charset=UTF-8" \
+        -a "Message-Id: ${MESSAGE_ID}" \
+        discuss@lists.freertc.org
+
+cat mailing-lists.txt | cut -f1 -d, | while read ;
+do
+
+  cat $MSG_BODY | \
+    mail -s "Fwd: $SUBJECT" \
+        -r "fosdem-rtc-admin@freertc.org" \
+        -a "From: FOSDEM RTC Team <fosdem-rtc-admin@freertc.org>" \
+        -a "Content-Type: text/plain; charset=UTF-8" \
+        -a "References: ${MESSAGE_ID}" \
         "$REPLY"
 
 done
